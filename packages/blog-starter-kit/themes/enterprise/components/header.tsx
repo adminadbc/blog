881f322c8b6fd3a45bfb8aa8b1f5612a64310041
sideeq12 <abdwaheed2018@gmail.com>
2024-04-25 18:08:53 +0100

    logo fix

--- packages/blog-starter-kit/themes/enterprise/components/header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/header.tsx
@@ -1,36 +1,19 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { useState } from 'react';
+import Image from 'next/image';
 import { PublicationNavbarItem } from '../generated/graphql';
-import { Button } from './button';
-import { Container } from './container';
-import { useAppContext } from './contexts/appContext';
-import HamburgerSVG from './icons/svgs/HamburgerSVG';
-import { PublicationLogo } from './publication-logo';
-import PublicationSidebar from './sidebar';
-
-function hasUrl(
-	navbarItem: PublicationNavbarItem,
-): navbarItem is PublicationNavbarItem & { url: string } {
-	return !!navbarItem.url && navbarItem.url.length > 0;
-}
+import Logo from "./newlogo.png"
 
 export const Header = () => {
 	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '/';
-	const [isSidebarVisible, setIsSidebarVisible] = useState<boolean>();
-	const { publication } = useAppContext();
-	const navbarItems = publication.preferences.navbarItems.filter(hasUrl);
-	const visibleItems = navbarItems.slice(0, 3);
-	const hiddenItems = navbarItems.slice(3);
 
 	const toggleSidebar = () => {
 		setIsSidebarVisible((prevVisibility) => !prevVisibility);
 	};
 
-
-
 	return (
-		<header className="border-b border-2">
-		Header
+		<header className="border-b border-2 flex pl-10 py-5">
+		<Image src={Logo} height={200} width={300} alt="" />
 		</header>
 	);
 };
