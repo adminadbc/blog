eb36a6b14c407587fa37ce7dd899f0ee347c17f1
Rashad Bayram <BRashad@users.noreply.github.com>
2024-04-25 21:29:27 -0600

    menu added

--- packages/blog-starter-kit/themes/enterprise/components/meta.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/meta.tsx
@@ -7,7 +7,7 @@ export const Meta = () => {
 	const { metaTags, favicon } = publication;
 	const defaultFavicons = (
 		<>
-			<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+			{/* <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" /> */}
 			<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
 			<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
 			<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
