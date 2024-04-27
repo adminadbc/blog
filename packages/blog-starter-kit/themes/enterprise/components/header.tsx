00f6e1d99e287c2f9f32c3a7c5a17aa1c19cde86
sideeq12 <abdwaheed2018@gmail.com>
2024-04-27 18:03:30 +0100

    descendant fix

--- packages/blog-starter-kit/themes/enterprise/components/header.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/header.tsx
@@ -171,15 +171,14 @@ export const Header = () => {
 							</li>
 						<li  onClick={()=>setSlideDown(!slideDown)}>
 							<Link href=""><h6>Initiatives</h6>
-						
-						{slideDown && <div className='text-sm bg-white shadow-lg p-3 flex  flex-col space-y-4 rounded-lg w-fit'>
+							</Link>
+							{slideDown && <div className='text-sm bg-white shadow-lg p-3 flex  flex-col space-y-4 rounded-lg w-fit'>
 					<Link href="https://blog.abcfoundationconnect.com">
 							<h6>Legal Communities Connects</h6></Link>
 							<Link href="https://www.abcfoundationconnect.com/main/resources/education">
-							<h6>				name: 'Changemakers',
+							<h6>Changemakers
 </h6></Link>
 							</div>}
-							</Link>
 							</li>
 			
 						<li><Link href="https://www.abcfoundationconnect.com/main"><h6>Contact Us</h6></Link></li>
