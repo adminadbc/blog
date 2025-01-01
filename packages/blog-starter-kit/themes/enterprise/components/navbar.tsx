410bd2d338ad2046c72e6900e06ffb592f278ccd
Rashad Bayram <BRashad@users.noreply.github.com>
2024-12-31 20:34:44 -0700

    updated to new navbar

--- packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/navbar.tsx
@@ -1,11 +1,12 @@
-import { Search } from './searchbar';
+import { useAppContext } from './contexts/appContext';
 import { SocialLinks } from './social-links';
 
 export const Navbar = () => {
-	return (
-		<div className="grid grid-cols-1 items-center gap-5 pt-5 text-sm md:grid-cols-2">
-		
-			<SocialLinks />
-		</div>
-	);
+  const { publication } = useAppContext();
+  
+  return (
+    <div className="grid grid-cols-1 items-center gap-5 pt-5 text-sm md:grid-cols-2">
+      <SocialLinks />
+    </div>
+  );
 };
